1ec5316497123c9719cfa8bcabf6f786f542c652
faalbers <chalbers@gmail.com>
2021-12-18 22:25:13 -0800

    last fix , all good now

--- TestBox.cpp
+++ TestBox.cpp
@@ -22,15 +22,8 @@ void TestBox::modelChanged()
 
 void TestBox::updateCurvePath_()
 {
-    Eigen::Array4Xd vertices =
-        cameraModel_->getMatrix("project") *
-        curveNode_->getTransformTo(cameraNode_) *
-        curveModel_->getPointArray("vtx").matrix();
-    for ( auto vertex : vertices.colwise() ) vertex /= vertex(3);
-
-    Eigen::Matrix4d screenMM = MH::ModelHierachy::screenTransform(
-        CURVEPLOT_SCENE_WIDTH, -CURVEPLOT_SCENE_HEIGHT);
-    vertices = screenMM * vertices.matrix();
+    auto vertices = curveModel_->getPointArray("vtx");
+    MH::ModelHierachy::pointsToScreen(curveNode_, cameraNode_, vertices);
 
     pathPoints_.clear();
     for ( size_t index = 0; index < vertices.cols(); index++ )
